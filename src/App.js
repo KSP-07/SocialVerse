20f071749826cf4461e7d2b468d19dfdf7d26c5a
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2022-10-19 12:01:06 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import React, { useContext } from "react";
+import {  BrowserRouter as Router,  Routes,  Route,Navigate} from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+import Messenger from "./pages/messenger/Messenger";
+import TodoList from "./pages/todo/components/TodoList";
+// import index from "./pages/Ecommerce/Shop";
+import Index from "./pages/Ecomerce/Index";
 function App() {
+  // return <Home/>
+  const {user} = useContext(AuthContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        {/* in place os switch now Routes is used */}
+        <Route exact path="/" element={user?<Home/> : <Register/>}/>
+        <Route  path="/login" element={user?  <Navigate to="/" />  :<Login/>}/>
+        <Route  path="/register" element={user?<Navigate to="/" /> :<Register/>}/>
+        {/* added this new line */}
+        <Route  path="/messenger" element={!user?<Navigate to="/" /> :<Messenger/>}/>
+        {/* :) */}
+        <Route  path="/profile/:username" element={<Profile/>}/>
+        <Route path="/todo" element={user ? <TodoList /> : <Navigate to="/" />} />
+        <Route path="/ecommerce" element={user ? <Index /> : <Navigate to="/" />} />
+      </Routes>
+    </Router> 
+  )
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*  wrapping this app in authcontext provider */}
+    <AuthContextProvider>
+      {/* this app in now our children (see in authcontext.js)   .. so we are sharing all these values like user, isFetching, error of authcontext in <APP> here */}
+      <App />
+    </AuthContextProvider>      
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  
